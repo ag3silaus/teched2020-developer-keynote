8fa87da20e8077a858cb40b1388fd420217297ff
Burak Yilmaz <58527608+ag3silaus@users.noreply.github.com>
2021-04-15 13:46:17 +0200

    Update docker_publish.sh

--- converter/docker_publish.sh
+++ converter/docker_publish.sh
@@ -1,1 +1,1 @@
- docker push docker.pkg.github.com/sap-samples/teched2020-developer-keynote/calculationservice:latest
+ docker push docker.pkg.github.com/ag3silaus/teched2020-developer-keynote/calculationservice:latest
