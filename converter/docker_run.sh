ca77d99c6d8269574127e53c8d9f99b5f02442cd
Burak Yilmaz <58527608+ag3silaus@users.noreply.github.com>
2021-04-15 13:46:36 +0200

    Update docker_run.sh

--- converter/docker_run.sh
+++ converter/docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 docker.pkg.github.com/sap-samples/teched2020-developer-keynote/calculationservice:latest
+docker run -d -p 8080:8080 docker.pkg.github.com/ag3silaus/teched2020-developer-keynote/calculationservice:latest
